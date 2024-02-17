8305087a1bd4da818497434c5c5b928890bdd29e
Parvinder <techpinda1214@gmail.com>
2024-02-17 20:44:23 +0530

    Adding Index page configuring urls and views.py

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+#from .forms import SignUpForm, AddRecordForm
+from django.contrib.auth.decorators import login_required
+#from .models import Record
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
