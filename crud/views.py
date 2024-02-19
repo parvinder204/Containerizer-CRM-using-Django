be8e3aaae8ce5abff44bf3ed1f1acb0b629e26a0
Parvinder <techpinda1214@gmail.com>
2024-02-19 10:35:49 +0530

    Workflow and Authentication pages

--- crud/views.py
+++ crud/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-#from .forms import SignUpForm, AddRecordForm
+from .forms import SignUpForm # AddRecordForm
 from django.contrib.auth.decorators import login_required
 #from .models import Record
 
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have Successfully Register!")
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()
+        return render(request, 'signup.html', {'form':form})
+
+    return render(request, 'signup.html', {'form':form})
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been Logged in!")
+            return redirect('home')
+        else:
+            messages.success(request,"There was an Error!")
+            return redirect('signin')
+        
+    else:
+        return render(request, 'signin.html')
