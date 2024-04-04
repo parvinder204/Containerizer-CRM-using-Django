08c9ad01d2a5848bb6a3243e6206561d0571b3ab
Parvinder <techpinda1214@gmail.com>
2024-04-05 03:53:32 +0530

    Almost Done!

--- crud/urls.py
+++ crud/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('home',views.home, name='home'),
     path('report',views.report,name='report'),
     path('add_record',views.add_record,name='add_record'),
+    path('record/<int:pk>', views.customer_record, name='record'),
+    path('signout',views.signout, name='signout'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -76,3 +76,39 @@ def add_record(request):
 	else:
 		messages.success(request, "You Must Be Logged In...")
 		return redirect('home') 
+     
+@login_required(login_url='signin')
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# Look Up Records
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
+      
+@login_required(login_url='signin')
+def update_record(request, pk):
+	if request.user.is_authenticated:
+		current_record = Record.objects.get(id=pk)
+		form = AddRecordForm(request.POST or None, instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Record Has Been Updated!")
+			return redirect('home')
+		return render(request, 'update_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
+
+
+@login_required(login_url='signin')
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
